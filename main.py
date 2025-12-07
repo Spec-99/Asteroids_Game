391a3861f28937f54836346317d3f8c9ce937190
Timur Fergione <Fergionet@gmail.com>
2025-12-07 11:28:24 -0500

    finished gameloop. started player.

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import pygame
+from constants import *
+from logger import log_state
+
+pygame.init()
+
+
+clock = pygame.time.Clock()
+dt = 0 
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
 def main():
-    print("Hello from asteroids-game!")
+    print(f"Starting Asteroids")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
     main()
+
+    
